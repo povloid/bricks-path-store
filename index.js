169fe8d6d197bb808cff03265f8b4c2626da01c8
Pavel Kopychenko <t34box@gmail.com>
2021-10-22 20:56:50 +0300

    Code format.

--- index.js
+++ index.js
@@ -21,51 +21,49 @@ const { assign_, get, set_, thread, update_ } = require('bricks-fp')
 // }
 
 export default class PathStore {
-  constructor(initState) {
-    this.state = initState
-    this.listeners = {}
-    this.keySeq = 0
-  }
+    constructor(initState) {
+        this.state = initState
+        this.listeners = {}
+        this.keySeq = 0
+    }
 
-  addChangeStateListener(fun) {
-    const key = this.keySeq++
-    this.listeners[key] = fun
-    return key
-  }
+    addChangeStateListener(fun) {
+        const key = this.keySeq++
+        this.listeners[key] = fun
+        return key
+    }
 
-  removeChangeStateListener(key) {
-    delete this.listeners[key]
-  }
+    removeChangeStateListener(key) {
+        delete this.listeners[key]
+    }
 
-  runChageListeners(path) {
-    Object.values(this.listeners).forEach(fun =>
-      fun({ state: this.state, path })
-    )
-  }
+    runChageListeners(path) {
+        Object.values(this.listeners).forEach((fun) => fun({ state: this.state, path }))
+    }
 
-  getState() {
-    return this.state
-  }
+    getState() {
+        return this.state
+    }
 
-  getStateIn(path, defaultValue) {
-    return get(this.state, path, defaultValue)
-  }
+    getStateIn(path, defaultValue) {
+        return get(this.state, path, defaultValue)
+    }
 
-  updateState(fun, ...args) {
-    this.state = thread({}, assign_(this.state), o => fun(o, ...args))
-    this.runChageListeners([])
-    return this
-  }
+    updateState(fun, ...args) {
+        this.state = thread({}, assign_(this.state), (o) => fun(o, ...args))
+        this.runChageListeners([])
+        return this
+    }
 
-  setStateIn(path, value) {
-    this.state = thread({}, assign_(this.state), set_(path, value))
-    this.runChageListeners(path)
-    return this
-  }
+    setStateIn(path, value) {
+        this.state = thread({}, assign_(this.state), set_(path, value))
+        this.runChageListeners(path)
+        return this
+    }
 
-  updateStateIn(path, fun, ...args) {
-    this.state = thread({}, assign_(this.state), update_(path, fun, ...args))
-    this.runChageListeners(path)
-    return this
-  }
+    updateStateIn(path, fun, ...args) {
+        this.state = thread({}, assign_(this.state), update_(path, fun, ...args))
+        this.runChageListeners(path)
+        return this
+    }
 }
